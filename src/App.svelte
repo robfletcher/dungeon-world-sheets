215a38357a9eacfaa9abc8e02ab8bb676569fabc
Rob Fletcher <rfletcher@netflix.com>
2020-03-06 12:57:51 -0800

    bind stat blocks to a larger model

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,55 @@
 <script>
-	import Stat from "./Stat.svelte";
-	export let name;
+  import Stat from "./Stat.svelte";
+
+  export let name;
+
+  export let character = {
+    name: "",
+    stats: {
+      strength: 16,
+      dexterity: 15,
+      constitution: 13,
+      intelligence: 12,
+      wisdom: 10,
+      charisma: 8
+    }
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>{character.name}</h1>
 </main>
 
+<label>Name <input bind:value={character.name}></label>
+<label>Class <select></select></label>
+
 <section id="stats">
-	<Stat name="Strength" debility="Weak"/>
-	<Stat name="Dexterity" debility="Shaky"/>
-	<Stat name="Constitution" debility="Sick"/>
-	<Stat name="Intelligence" debility="Stunned"/>
-	<Stat name="Wisdom" debility="Confused"/>
-	<Stat name="Charisma" debility="Scarred"/>
+  <Stat name="Strength" debility="Weak" bind:score={character.stats.strength}/>
+  <Stat name="Dexterity" debility="Shaky" bind:score={character.stats.dexterity}/>
+  <Stat name="Constitution" debility="Sick" bind:score={character.stats.constitution}/>
+  <Stat name="Intelligence" debility="Stunned" bind:score={character.stats.intelligence}/>
+  <Stat name="Wisdom" debility="Confused" bind:score={character.stats.wisdom}/>
+  <Stat name="Charisma" debility="Scarred" bind:score={character.stats.charisma}/>
 </section>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/Stat.svelte
+++ src/Stat.svelte
@@ -1,16 +1,22 @@
 <script>
-    export let name;
-    export let debility;
-    export let score = 3;
-    let bonus;
-    let isDebilitated = false;
+  export let name;
+  export let debility;
+  export let score;
+  let bonus;
+  let isDebilitated = false;
 
-    $: bonus = (score == 3 ? -3 : score <= 5 ? -2 : score <= 8 ? -1 : score <= 12 ? 0 : score <= 15 ? 1 : score <= 17 ? 2 : 3) - (isDebilitated ? 1 : 0);
+  $: bonus = (score === 3 ? -3 : score <= 5 ? -2 : score <= 8 ? -1 : score <= 12 ? 0 : score <= 15 ? 1 : score <= 17 ? 2 : 3) - (isDebilitated ? 1 : 0);
 </script>
 
+<style>
+  .bonus-positive:before {
+    content: '+';
+  }
+</style>
+
 <section>
-    <h3>{name}</h3>
-    <input type="number" min="3" max="18" bind:value={score}>
-    <label>{debility} <input type="checkbox" bind:checked={isDebilitated}></label>
-    <div>{bonus}</div>
+  <h3>{name}</h3>
+  <input type="number" min="3" max="18" bind:value={score}>
+  <label>{debility} <input type="checkbox" bind:checked={isDebilitated}></label>
+  <div class:bonus-positive={bonus >= 0}>{bonus}</div>
 </section>

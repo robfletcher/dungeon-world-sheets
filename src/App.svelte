2c7a13866c37e54c7407dc0ae0107a4b5566c9f0
Rob Fletcher <rfletcher@netflix.com>
2020-03-06 06:17:22 -0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Stat from "./Stat.svelte";
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<section id="stats">
+	<Stat name="Strength" debility="Weak"/>
+	<Stat name="Dexterity" debility="Shaky"/>
+	<Stat name="Constitution" debility="Sick"/>
+	<Stat name="Intelligence" debility="Stunned"/>
+	<Stat name="Wisdom" debility="Confused"/>
+	<Stat name="Charisma" debility="Scarred"/>
+</section>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Stat.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let name;
+    export let debility;
+    export let score = 3;
+    let bonus;
+    let isDebilitated = false;
+
+    $: bonus = (score == 3 ? -3 : score <= 5 ? -2 : score <= 8 ? -1 : score <= 12 ? 0 : score <= 15 ? 1 : score <= 17 ? 2 : 3) - (isDebilitated ? 1 : 0);
+</script>
+
+<section>
+    <h3>{name}</h3>
+    <input type="number" min="3" max="18" bind:value={score}>
+    <label>{debility} <input type="checkbox" bind:checked={isDebilitated}></label>
+    <div>{bonus}</div>
+</section>
